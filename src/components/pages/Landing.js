6b773d3cc2f5a4e30f4e68be53fd43c0d3e7eead
Ava <avamsteimle@gmail.com>
2022-06-24 16:49:38 -0500

    animation added to landing

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -11,11 +11,20 @@ export default function Landing({currentPage, handlePageChange}) {
              style={{height: "245px", width: "340px", margin: "auto"}}>
             <img src={landingPic1} alt="creative black and white head shot" style={{height: "100%", width: "100%", objectFit: "contain"}}/> 
             </div>
-            <div
+            {/* <div
             style={{width: "100%", margin: "auto", display: "flex", justifyContent: "center", paddingTop: "15px"}}>
             <h1
             style={{fontWeight: "bold"}}>HANNAH WILSON.</h1>
-            </div>
+            </div> */}
+            <div 
+            style={{width: "100%", margin: "auto", display: "flex", justifyContent: "center", paddingTop: "15px"}}
+            className="container">
+  <div className="row">
+    <div className="col-md-12 text-center">
+      <h1 className="animate-charcter">HANNAH WILSON.</h1>
+    </div>
+  </div>
+</div>
             <div
              style={{height: "245px", width: "340px", margin: "auto"}}>
             <img src={landingPic2} alt="creative black and white head shot" style={{height: "100%", width: "100%", objectFit: "contain"}}/> 
