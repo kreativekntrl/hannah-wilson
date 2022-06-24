e6d8fa467a7f0a052919f3a2e6455970f9bb8c98
Ava <avamsteimle@gmail.com>
2022-06-23 20:26:52 -0500

    nav bar updated w/ accurate page links

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,9 +3,17 @@ import React from 'react';
 function NavTabs({ currentPage, handlePageChange }) {
   return (
 <nav>
+ <div>
   <a
   href='#Gallery'
-  onClick={() => handlePageChange('Gallery')}>ENTER.</a>
+  onClick={() => handlePageChange('Gallery')}>Gallery.</a>
+  <a
+  href='#Measurements'
+  onClick={() => handlePageChange('Measurements')}>Measurements.</a>
+  <a
+  href='#Landing'
+  onClick={() => handlePageChange('Landing')}>Exit.</a>
+</div>
 </nav>
   );
 }
